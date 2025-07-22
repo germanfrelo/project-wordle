763421c93f12c2c578eec5f27c415e0f39719863
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-07-22 19:59:27 +0200

    chore: Opt into React "Strict Mode" (#72)
    * Use React's Strict Mode
    
    * Remove unnecessary React imports
    
    Reference: https://react.dev/reference/react/StrictMode

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { checkGuess } from "../../game-helpers";
 import { range } from "../../utils";
 
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { range } from "../../utils";
 import Guess from "../Guess/Guess";
