57f3d7f7576e3ef1e25819aa40503dbf0d150a8f
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2025-07-22 19:59:27 +0200

    chore: Opt into React "Strict Mode" (#72)
    * Use React's Strict Mode
    
    * Remove unnecessary React imports
    
    Reference: https://react.dev/reference/react/StrictMode

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 function Banner({ status = "", children }) {
 	return <div className={`banner ${status}`}>{children}</div>;
 }
--- src/components/BannerLose/BannerLose.js
+++ src/components/BannerLose/BannerLose.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Banner from "../Banner/Banner";
 
 function BannerLose({ answer }) {
